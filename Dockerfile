8c515c35613f833d77b7488a3b3771157a974ef7
sreenihari <HARI0030@e.ntu.edu.sg>
2023-01-28 20:40:11 +0800

    Changes to docker/azure versions

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM python:3.9
 MAINTAINER Sreeni Hari "sreeni.hari@gmail.com"
 COPY . /app
 WORKDIR /app
+ADD app.py.dev app.py
 RUN pip install -r requirements.txt
+EXPOSE 5000
 ENTRYPOINT ["python"]
 CMD ["app.py"]
--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return 'Hello world!'
+@app.route("/")
+def hello():
+    return "Flask running as a Azure app!!"
+
+if __name__ == "__main__":
+   app.run(debug=True)
+        
